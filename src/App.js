8e4766bc0e13d17c593a6e8b058bca1e27a4cec7
Amol Dogra <amol88658@gmail.com>
2021-12-17 00:16:25 +0530

    Published by using Git Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 
 import './App.css';
 // import Alert from './components/Alert';
- import About from './components/About';
+//  import About from './components/About';
  import Navbar from './components/Navbar';
  import TextForm from './components/TextForm';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+
+// } from "react-router-dom";
 
 function App() {
   return (
-    <>
+     <>
+      {/* <Router> */}
       <Navbar  title ="TextUtils" about="About Us"/>
       {/* <Alert alert="bfjd,nc"/> */}
       <div className='container my-3'>
-      <TextForm heading="Enter the Text Analyze below"/>
+      {/* <Switch>
+          <Route exact path="/about">
+            <About />                                       // navigate from about to home
+          </Route>
+          <Route exact path="/">
+          
+          </Route>
+        </Switch> */}
+        <TextForm heading="Enter the Text Analyze below"/>
       </div> 
-      <About/> 
+      {/* </Router> */}
+      
+      
 </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// import { Link,  } from 'react-router-dom'
+
 
 export default function Navbar(props) {
     return (
@@ -8,18 +10,22 @@ export default function Navbar(props) {
    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
+    {/* <Link className="navbar-brand" a="/">{props.title}</Link> */}
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active"  href="/">Home</a>
+          {/* <Link className="nav-link active"  a="/">Home</Link>             // For Routing */}
         </li>
        
         
         <li className="nav-item">
-          <a className="nav-link disabled" href="/">{props.about}</a>
+          <a className="nav-link active" href="/about">{props.about}</a>
+          {/* <Link className="nav-link active" a="/about">{props.about}</Link> */}
+
         </li>
       </ul>
       {/* <form className="d-flex">
